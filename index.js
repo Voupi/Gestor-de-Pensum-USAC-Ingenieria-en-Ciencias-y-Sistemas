59c3a906c734021063e0da137a282476a4078c9b
Voupi <voupi.sfh@gmail.com>
2024-01-08 13:19:52 -0600

    Avance en el tiempo aplicado y mejoras en la lectura del arhivo

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const { inquirerMenu,
     listadoTareasCompletar
 } = require('./helpers/inquirer');
 
-const Cursos = require('./models/cursos')
+const Cursos = require('./models/cursos');
+const { down } = require('inquirer/lib/utils/readline');
 
 const main = async() => {
     let Option = 0;
@@ -31,6 +32,37 @@ const main = async() => {
             case 4: //Listar Cursos Desbloqueados
                 cursos.listarCursosPendientesObligatorios();
             break;
+            case 5: //Listar Cursos Desbloqueados
+                let tempCursosDesbloqueadosObligatorios = []
+                cursos._cursosDesbloqueados.forEach( curso => {
+                    if (curso.Obligatorio == "*") {
+                        tempCursosDesbloqueadosObligatorios.push(curso)
+                    }
+                })
+                const codigosCursos = await listadoTareasCompletar(tempCursosDesbloqueadosObligatorios)
+                console.log(codigosCursos)
+                cursos.aprobarCursosPendientes(codigosCursos);
+            break;
+            case 6: //Hacer una linea del tiempo de cursos por periodo de tiempo
+                countPeriodo = 1;
+                let indexCursos, countCursos;
+                do {
+                    indexCursos = [];
+                    countCursos = 0;
+                    //console.log(indexCursos.length + 'Tamaño del array')
+                    console.log('\n Periodo de Tiempo número: '+countPeriodo +'\n')
+                    cursos.listarCursosDesbloqueados();
+                    cursos._cursosDesbloqueados.forEach( curso => {
+                        if (curso.Obligatorio == "*") {
+                            indexCursos.push(curso.index);
+                            countCursos++
+                        }
+                    })
+                    console.log('\n Total de Cursos: '+countCursos)
+                    cursos.aprobarCursosPendientes(indexCursos);
+                    countPeriodo++
+                } while (indexCursos.length != 0 && countPeriodo < 15);
+                break;
             default:
                 /*for (let index = 0; index < 10; index++) {
                     //const element = array[index];
